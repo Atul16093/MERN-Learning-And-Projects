df6ab6fff2cdd57528b7a9a18d9cac0ea27c63e0
Atul16093 <atulrag3@gmail.com>
2025-01-26 16:41:10 +0530

    component life cycle
    Here we disscussed about mounting , unmounting , updating and there're some method to deal with the data

---
+++ React-Learning/componentlifecycle/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import {Component} from 'react';
+import './App.css';
+
+class App extends Component{
+   constructor(){
+    super();
+    console.log("Constructor rendering");
+    this.state = {
+        count : 100
+    }
+   }
+   componentDidMount(){
+      console.log("ComponenetDidMount render here ");
+   }
+   componentDidUpdate(){
+    console.log("Component update render");
+   }
+   componentWillUnmount(){
+    console.log("ComponenetWillUnMount");
+    
+   }
+   render(){
+    console.log("Render method....");
+     return <>
+         <button className='btn btn-info' onClick={()=>{this.setState({count : this.state.count+1})}}>Button {this.state.count}</button>
+      </>
+   }
+}
+
+export default App;
