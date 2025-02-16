57dd34dd572f94fc45c8ad4b30a22b39e8da1536
Atul16093 <atulrag3@gmail.com>
2025-02-16 10:56:57 +0530

    Radio button and validation

--- React-Learning/FunctionalComponents/FComponent/src/App.jsx
+++ React-Learning/FunctionalComponents/FComponent/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 // import Data from './components/Data'
 import { useState } from 'react'
-import Display from './components/Display.jsx';
 import Header from './components/Header.jsx';
 import StudentAdd from './components/StudentAdd.jsx';
 function App() {
--- React-Learning/FunctionalComponents/FComponent/src/components/StudentAdd.jsx
+++ React-Learning/FunctionalComponents/FComponent/src/components/StudentAdd.jsx
@@ -1,8 +1,9 @@
 import { useRef , useState } from "react";
-import Display from "./Display.jsx";
 import Data from "./Data";
 function studentAdd(){
-    const[student , setStudent] = useState(null);
+    const[student , setStudent] = useState(Data);
+    const[defaultList , setDefaultList] = useState("All");
+    const[error , setError] = useState();
     const branch = ["IT" , "CV" , "CS" , "Mech"];
     const rollRef = useRef(null);
     const nameRef = useRef(null);
@@ -14,30 +15,57 @@ function studentAdd(){
         const gender = genderRef.current.value;
         const branch = branchRef.current.value;
         const studentinfo = {roll , name , gender , branch};
-        console.log(studentinfo);
+        setStudent([...student , studentinfo]);
+    }
+    const male = ()=>{
+        const male = student.filter((individual)=>{return individual.gender == "Male"});
+        console.log(male);
+        setStudent(male);
+    }
+    const female = ()=>{
+        const female = student.filter((individual)=>{return individual.gender == "Female"});
+        console.log(female);
+        setStudent(female)
+    }
+    const All = ()=>{
+        setStudent(Data)
+    }
+    
+    const identify = ()=>{
+        let roll = rollRef.current.value;
+        const unique = student.findIndex((check)=>{return check.roll == roll});
+
+        if(unique === -1){
+            setError("");
+        }else{
+            let message = "This roll no is already taken"
+            setError(message)
+        }
         
-        setStudent(studentinfo);
     }
     return <>
+
         <div className="container m-2 ">
             <div className="row">
                 <div className="col-md-6">
-                    <input ref={rollRef} id = "roll" className="form-control" type="text" placeholder="Enter your Roll No" />
+                    <input onKeyUp={identify} ref={rollRef} id = "roll" className="form-control" type="text" placeholder="Enter your Roll No" />
+                    <p className="text-danger">{error}</p>
                 </div>
                 <div className="col-md-6 ">
-                    <input ref={nameRef} className="form-control" type="text" placeholder="Enter your Name" />
+                    {/* error !== ""  */}
+                    <input ref={nameRef} className="form-control" type="text" placeholder="Enter your Name" disabled = {error !== ""}/>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-6 mt-2">
-                 <select ref={genderRef}  className="form-control" name="gender" id="">
+                 <select  disabled = {error !== ""} ref={genderRef}  className="form-control" name="gender" id="">
                     <option value="0">Select Gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                  </select>
                 </div>
                 <div className="col-md-6 mt-2">
-                  <select ref={branchRef} className="form-control" name="branch" id="">
+                  <select  disabled = {error !== ""} ref={branchRef} className="form-control" name="branch" id="">
                     <option value="0">Select branch</option>
                     {branch.map((branch , index)=>{return <option key={index}>
                         {branch}
@@ -47,17 +75,49 @@ function studentAdd(){
             </div>
             <div className="row">
                 <div className="col-md-6 mt-3">
-                <button onClick={addingStudent} className="btn  btn-success">Add</button>
+                <button disabled = {error !==""} onClick={()=>{ addingStudent()}} className="btn  btn-success">Add</button>
                 </div>
                 <div className="col-md-6 mt-3">
-                <button onClick=" " className="btn m-2  btn-outline-success" style={{width : 80}}>IT</button>
-                <button onClick=" " className="btn m-2 btn-outline-secondary" style={{width : 80}}>CV</button>
-                <button onClick=" " className="btn m-2 btn-outline-warning" style={{width : 80}}>Mech</button>
-                <button onClick=" " className="btn m-2 btn-outline-primary" style={{width : 80}}>CS</button>
+                <button onClick={()=>{setDefaultList("IT")}} className="btn m-2  btn-outline-success" style={{width : 80}}>IT ({student.filter((filtering)=>{return filtering.branch == "IT"}).length})</button>
+                <button onClick={()=>{setDefaultList("CV")}} className="btn m-2 btn-outline-secondary" style={{width : 80}}>CV ({student.filter((filtering)=>{return filtering.branch == "CV"}).length})</button>
+                <button onClick={()=>{setDefaultList("Mech")}} className="btn m-2 btn-outline-warning" style={{width : 90}}>Mech ({student.filter((filtering)=>{return filtering.branch == "Mech"}).length})</button>
+                <button onClick={()=>{setDefaultList("CS")}} className="btn m-2 btn-outline-primary" style={{width : 80}}>CS ({student.filter((filtering)=>{return filtering.branch == "CS"}).length})</button>
+                <button onClick={()=>{setDefaultList("All")}} className="btn m-2 btn-outline-info" style={{width : 80}}>All ({student.length})</button>
                 </div>
             </div>
         </div>
-        <Display data = {student}/>
+        <div className="row m-2">
+            <div className="col-md-6">
+                <div>
+                    <label >Gender : </label>
+                    <input onClick={male} className="m-2" type="radio" id="male" name="gender"  />
+                    <label htmlFor="male">Male</label>
+                    <input onClick = {female} className="m-2" type="radio" id="female" name="gender" />
+                    <label htmlFor="female">Female</label>
+                    <input onClick={All} className="m-2" type="radio" id="all" name="gender" />
+                    <label htmlFor="all">All</label>
+
+               </div>
+            </div>
+        </div>
+        <table  className="table table-bordered">
+                 <thead>
+                   <tr>
+                    <th>ROll NO</th>
+                    <th>Name </th>
+                    <th>Gender</th>
+                    <th>Branch</th>
+                </tr>
+            </thead>
+            <tbody>
+                {student.filter((filtered)=>{ return filtered.branch == defaultList || defaultList == "All"}).map((individual , index)=>{ return <tr key={index}>
+                    <td>{individual.roll}</td>
+                    <td>{individual.name}</td>
+                    <td>{individual.gender}</td>
+                    <td>{individual.branch}</td>
+                </tr>})}
+            </tbody>
+        </table>
     </>
 }
 
